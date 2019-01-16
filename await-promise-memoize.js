0603ca81896ffc713b0f8f1e8147c6399f7040b9
haiduchyk <haiduchyk.t@gmail.com>
2019-01-16 18:03:07 +0200

    first commit

---
+++ await-promise-memoize.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const fs = require('fs');
+
+const generateKey = args => (
+  args.map(x => x.toString() + ':' + typeof(x)).join('|')
+);
+
+const memoize = fn => {
+  const cache =  new Map();
+  return async (...args) => {
+    if (typeof(args[args.length - 1]) === 'function') {
+      const cb = args.pop();
+      const key = generateKey(args);
+      if (cache.has(key)) {
+        console.log('From cache');
+        cb(null, cache.get(key));
+      } else {
+        const promise = new Promise((resolve, reject) =>
+          fn(...args, (err, data) => {
+            if (err) reject(err);
+            else resolve(data);
+          }));
+          console.log('Calculate');
+          try {
+            const res = await promise;
+            cache.set(key, res);
+            cb(null, res);
+          } catch (err) {
+            cb(err, null)
+          }
+      }
+    } else {
+      const key = generateKey(args);
+      if (cache.has(key)) {
+        console.log('From cache:');
+        return cache.get(key);
+      }
+      const promise = new Promise((resolve, reject) =>
+        fn(...args, (err, data) => {
+          if (err) reject(err);
+          else resolve(data);
+        }));
+      console.log('Calculate:');
+      const res = await promise;
+      cache.set(key, res);
+      return res;
+    }
+  };
+};
